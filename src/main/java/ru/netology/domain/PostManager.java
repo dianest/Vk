f774702ce5e633a0e2184d22a3ec5aec2831fc9d
dianest <nddgdd@gmail.com>
2020-09-21 22:06:44 +0200

    Added PostManager

---
+++ src/main/java/ru/netology/domain/PostManager.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] searchPosts(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        return null;
+    }
+
+    public void deletePosts(int ownerId, int postId) {
+    }
+}
